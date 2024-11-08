8776299b2222d8a7c848e592743817a26b1c7891
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-08 15:12:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
 
+const sets = {
+  set1: {
+    1: {
+      letter: "Q",
+      id: "Chord-1",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+    },
+    2: {
+      letter: "W",
+      id: "Chord-2",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+    },
+    3: {
+      letter: "E",
+      id: "Chord-3",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+    },
+    4: {
+      letter: "A",
+      id: "Shaker",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+    },
+    5: {
+      letter: "S",
+      id: "Open-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+    },
+    6: {
+      letter: "D",
+      id: "Closed-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+    },
+    7: {
+      letter: "Z",
+      id: "Punchy-Kick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+    },
+    8: {
+      letter: "X",
+      id: "Side-Stick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+    },
+    9: {
+      letter: "C",
+      id: "Snare",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+    }
+  },
+  set2: {
+    1: {
+      letter: "Q",
+      id: "Chord-1",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+    },
+    2: {
+      letter: "W",
+      id: "Chord-2",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+    },
+    3: {
+      letter: "E",
+      id: "Chord-3",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+    },
+    4: {
+      letter: "A",
+      id: "Shaker",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+    },
+    5: {
+      letter: "S",
+      id: "Open-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+    },
+    6: {
+      letter: "D",
+      id: "Closed-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+    },
+    7: {
+      letter: "Z",
+      id: "Punchy-Kick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+    },
+    8: {
+      letter: "X",
+      id: "Side-Stick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+    },
+    9: {
+      letter: "C",
+      id: "Snare",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+    }
+  }
+};
+
 function App() {
   return (
     <div className="App">
