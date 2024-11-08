583a85092965cdf889b2cee063c1232733486406
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-08 18:30:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,54 +4,6 @@ import { useEffect, useState } from 'react';
 
 const sets = {
   set1: {
-    name: "Smooth Piano Kit",
-    1: {
-      letter: "Q",
-      id: "Chord-1",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
-    },
-    2: {
-      letter: "W",
-      id: "Chord-2",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
-    },
-    3: {
-      letter: "E",
-      id: "Chord-3",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
-    },
-    4: {
-      letter: "A",
-      id: "Shaker",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
-    },
-    5: {
-      letter: "S",
-      id: "Open-HH",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
-    },
-    6: {
-      letter: "D",
-      id: "Closed-HH",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
-    },
-    7: {
-      letter: "Z",
-      id: "Punchy-Kick",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
-    },
-    8: {
-      letter: "X",
-      id: "Side-Stick",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
-    },
-    9: {
-      letter: "C",
-      id: "Snare",
-      link: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
-    }
-  },
-  set2: {
     name: "Heater Kit",
 
     1: {
@@ -100,14 +52,63 @@ const sets = {
       link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
     }
 
-  }
+  },
+  set2: {
+    name: "Smooth Piano Kit",
+    1: {
+      letter: "Q",
+      id: "Chord-1",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+    },
+    2: {
+      letter: "W",
+      id: "Chord-2",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+    },
+    3: {
+      letter: "E",
+      id: "Chord-3",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+    },
+    4: {
+      letter: "A",
+      id: "Shaker",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+    },
+    5: {
+      letter: "S",
+      id: "Open-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+    },
+    6: {
+      letter: "D",
+      id: "Closed-HH",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+    },
+    7: {
+      letter: "Z",
+      id: "Punchy-Kick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+    },
+    8: {
+      letter: "X",
+      id: "Side-Stick",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+    },
+    9: {
+      letter: "C",
+      id: "Snare",
+      link: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+    }
+  },
 };
 
 function App() {
 
-  const default_volume = 30
+  const default_volume = 30;
+  const default_set = sets.set1
 
-  const [setInUse, updateSet] = useState(sets.set2);
+  const [setInUse, updateSet] = useState(default_set);
   const [powered, updatePower] = useState(true);
   const [display, setDisplay] = useState("Welcome");
   const [volume, setVolume] = useState(default_volume);
@@ -125,26 +126,26 @@ function App() {
     setDisplay(setInUse.name)
   }
 
-  const playPad = (element,parent) => {
+  const playPad = (element, parent) => {
     if (element && powered) {
       element.currentTime = 0;
       element.volume = volume / 100;
       element.play();
 
-      setDisplay(parent.id.replace("-"," "));
+      setDisplay(parent.id.replace("-", " "));
     }
   };
 
   const handleClick = (event) => {
     const elementClicked = event.target;
     const audio = elementClicked.querySelector("audio");
-    playPad(audio,elementClicked);
+    playPad(audio, elementClicked);
   }
 
   const handleKeyDown = (event) => {
     const elemetPressed = document.querySelector(`[data-letter="${event.key.toUpperCase()}"]`);
     const audio = elemetPressed?.querySelector("audio");
-    playPad(audio,elemetPressed);
+    playPad(audio, elemetPressed);
   }
 
   useEffect(() => {
@@ -152,9 +153,7 @@ function App() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-
-  }
-  );
+  });
 
   return (
     <div className="App">
