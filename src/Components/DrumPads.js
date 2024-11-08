0066b1536cc59bc1e7ea3d933aaa9c50ceb5aa57
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-11-08 17:37:20 +0800

    Update DrumPads.js

--- src/Components/DrumPads.js
+++ src/Components/DrumPads.js
@@ -1,16 +1,16 @@
-function Drumpads({id, link, letter,onClick}) {
-
+function Drumpads({ id, link, letter, onClick }) {
     return (
         <div
+            key={id}
             id={id}
             data-letter={letter}
             onClick={onClick}
         >
             <audio
+                key={letter}
                 id={letter}
                 src={link}
             >
-
             </audio>{letter}
         </div>
     );
